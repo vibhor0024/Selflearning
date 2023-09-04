c942af9f01eb48b113ea67a5bbba411dd8859d70
vibhor024 <vibhorseth55@gmail.com>
2023-09-03 21:04:52 -0700

    rock paper scissors game

---
+++ rock paper scissors.py
@@ -0,0 +1,10 @@
+import random
+def get_choices():
+    player_choice = input('Enter a choice (rock, paper, scissors)')
+    options = ['rock','paper','scissors']
+    computer_choice = random.choice(options) 
+    choices = {'player':player_choice, 'computer': computer_choice}
+    return choices
+
+choices = get_choices()
+print(choices)
