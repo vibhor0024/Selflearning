d513e762a763cbaf60adc9e84a678a49b31a2158
vibhor024 <vibhorseth55@gmail.com>
2023-09-03 21:47:45 -0700

    rock paper scissors game updated

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -6,5 +6,26 @@ def get_choices():
     choices = {'player':player_choice, 'computer': computer_choice}
     return choices
 
+def get_winner(player,computer):
+    print(f'You chose {player}, and the computer chose {computer}')
+    if player == computer:
+        return 'tie'
+    elif player == 'rock':
+       if computer == 'paper':
+         return 'computer wins'
+       else:
+         return 'you win'
+    elif player == 'paper':
+       if computer == 'scissors':
+         return 'computer wins'
+       else:
+         return 'you win'
+    elif player == 'scissors':
+       if computer == 'rock':
+         return 'computer wins'
+       else:
+         return 'you win'
+
 choices = get_choices()
-print(choices)
+result = get_winner(choices['player'],choices['computer'])
+print(result)
