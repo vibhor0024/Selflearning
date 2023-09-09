f1aeb0e309a1c20ea906d817de8c4c53cd621cf7
vibhor024 <vibhorseth55@gmail.com>
2023-09-08 17:08:16 -0700

    dictionary methods

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+student = {'name':'Vibhor','age':24,'location':'Arizona','height':172}
+
+print(student.pop('age')) # to remove a specific key value pair
+
+print(student.popitem()) # removes the last item
+
+print(student)
+
+print(list(student.keys())) # list of all the keys
+
+print(list(student.values())) # list of all the values
+
+print(list(student.items())) # list of tuples with all key, value pairs
+
+for k,v in student.items():
+    print(k,v)
+
+
+studentcopy = student.copy() # to make a copy
+
+del student['name'] # deletes the name
+
+print(student)
